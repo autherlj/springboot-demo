8e3e487e2afdd18cb49885d6bf5052f618de2db2
chenzz <cs.chenzz@qq.com>
2018-10-21 15:39:22 +0800

    http://localhost:8080/,test接口

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # springboot-demo
+
 最简单的spring boot 示例,主要用来测试环境
+
+运行Application类和通过http://localhost:8080/访问测试
--- src/main/java/com/sibo/example/springbootdemo/web/TestController.java
+++ src/main/java/com/sibo/example/springbootdemo/web/TestController.java
@@ -2,19 +2,29 @@ package com.sibo.example.springbootdemo.web;
 
 import com.sibo.example.springbootdemo.core.R;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/test1")
-    public R test1() {
-        return R.ok().data("test");
+    @GetMapping("/test")
+    public R test() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "czz");
+        map.put("email", "cs.chenzz@qq.com");
+        map.put("address", "shenzhen gd,China");
+
+        return R.ok().data(map);
     }
 
-    @GetMapping("/test2")
-    public R test2() {
-        return R.error("发生错误");
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public R home() {
+        return R.ok().data("--------home page-----");
     }
 
 }
